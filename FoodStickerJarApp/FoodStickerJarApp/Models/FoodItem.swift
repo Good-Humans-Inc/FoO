a4fc373ea53175eb6af3134d2324c05d07a97ff9
Feng Yan <fengyan06080121@gmail.com>
2025-06-13 15:01:58 -0700

    updated gitignore

--- FoodStickerJarApp/FoodStickerJarApp/Models/FoodItem.swift
+++ FoodStickerJarApp/FoodStickerJarApp/Models/FoodItem.swift
@@ -1,13 +1,17 @@
 import Foundation
 import UIKit
+import FirebaseFirestore
 
 // Represents a single food sticker.
-// Codable: Allows us to encode/decode it to/from JSON for local storage.
+// Codable: Allows us to encode/decode it to/from JSON for local storage and Firestore.
 // Identifiable: Lets SwiftUI know how to uniquely identify each item in a list.
 // Equatable: Helps in finding and comparing items.
 // Hashable: Allows the item to be used as a unique identifier for the view.
 struct FoodItem: Identifiable, Codable, Equatable, Hashable {
-    let id: UUID
+    // By adding @DocumentID, Firestore automatically populates this property
+    // with the document's ID when data is fetched.
+    @DocumentID var id: String?
+    
     let creationDate: Date
     let imageData: Data
     let thumbnailData: Data
@@ -30,7 +34,8 @@ struct FoodItem: Identifiable, Codable, Equatable, Hashable {
 
     // A convenience initializer to create a FoodItem directly from a UIImage.
     init(image: UIImage) {
-        self.id = UUID()
+        // The ID will be nil until the item is saved to Firestore.
+        self.id = nil
         self.creationDate = Date()
         
         // We store the image as PNG data for persistence.
